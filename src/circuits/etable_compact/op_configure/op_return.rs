43a81b25169daa428f02a13716992fe15d6f1221
Zhang Junyu <junyu92@gmail.com>
2022-08-12 14:02:53 +0800

    add test test_trivial_return

--- src/circuits/etable_compact/op_configure/op_return.rs
+++ src/circuits/etable_compact/op_configure/op_return.rs
@@ -182,6 +182,19 @@ mod tests {
     use crate::test::test_circuit_builder::test_circuit_noexternal;
 
     #[test]
+    fn test_trivial_return() {
+        let textual_repr = r#"
+            (module
+                (func (export "test")
+                  return
+                )
+               )
+            "#;
+
+        test_circuit_noexternal(textual_repr).unwrap();
+    }
+
+    #[test]
     fn test_return_with_drop_ok() {
         let textual_repr = r#"
             (module
