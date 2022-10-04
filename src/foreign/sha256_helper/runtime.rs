d7f0a12718d77d9f8195ff937d986d027426032e
Zhang Junyu <junyu92@gmail.com>
2022-10-04 08:38:21 +0800

    bugfix: sha256: bit op should on u32

--- src/foreign/sha256_helper/runtime.rs
+++ src/foreign/sha256_helper/runtime.rs
@@ -4,45 +4,43 @@ use wasmi::{RuntimeArgs, RuntimeValue};
 use crate::runtime::host::HostEnv;
 
 fn lsigma0(args: RuntimeArgs) -> Option<RuntimeValue> {
-    let x: i32 = args.nth(0);
-    Some(RuntimeValue::I32(
-        x.rotate_right(2) ^ x.rotate_right(13) ^ x.rotate_right(22),
-    ))
+    let x: u32 = args.nth(0);
+    let res = x.rotate_right(2) ^ x.rotate_right(13) ^ x.rotate_right(22);
+    Some(RuntimeValue::I32(res as i32))
 }
 
 fn lsigma1(args: RuntimeArgs) -> Option<RuntimeValue> {
-    let x: i32 = args.nth(0);
-    Some(RuntimeValue::I32(
-        x.rotate_right(6) ^ x.rotate_right(11) ^ x.rotate_right(25),
-    ))
+    let x: u32 = args.nth(0);
+    let res = x.rotate_right(6) ^ x.rotate_right(11) ^ x.rotate_right(25);
+    Some(RuntimeValue::I32(res as i32))
 }
 
 fn ssigma0(args: RuntimeArgs) -> Option<RuntimeValue> {
-    let x: i32 = args.nth(0);
-    Some(RuntimeValue::I32(
-        x.rotate_right(7) ^ x.rotate_right(18) ^ (x >> 3),
-    ))
+    let x: u32 = args.nth(0);
+    let res = x.rotate_right(7) ^ x.rotate_right(18) ^ (x >> 3);
+    Some(RuntimeValue::I32(res as i32))
 }
 
 fn ssigma1(args: RuntimeArgs) -> Option<RuntimeValue> {
-    let x: i32 = args.nth(0);
-    Some(RuntimeValue::I32(
-        x.rotate_right(17) ^ x.rotate_right(19) ^ (x >> 10),
-    ))
+    let x: u32 = args.nth(0);
+    let res = x.rotate_right(17) ^ x.rotate_right(19) ^ (x >> 10);
+    Some(RuntimeValue::I32(res as i32))
 }
 
 fn ch(args: RuntimeArgs) -> Option<RuntimeValue> {
-    let x: i32 = args.nth(0);
-    let y: i32 = args.nth(1);
-    let z: i32 = args.nth(2);
-    Some(RuntimeValue::I32(z ^ (x & (y ^ z))))
+    let x: u32 = args.nth(0);
+    let y: u32 = args.nth(1);
+    let z: u32 = args.nth(2);
+    let res = z ^ (x & (y ^ z));
+    Some(RuntimeValue::I32(res as i32))
 }
 
 fn maj(args: RuntimeArgs) -> Option<RuntimeValue> {
-    let x: i32 = args.nth(0);
-    let y: i32 = args.nth(1);
-    let z: i32 = args.nth(2);
-    Some(RuntimeValue::I32((x & y) ^ (z & (x ^ y))))
+    let x: u32 = args.nth(0);
+    let y: u32 = args.nth(1);
+    let z: u32 = args.nth(2);
+    let res = (x & y) ^ (z & (x ^ y));
+    Some(RuntimeValue::I32(res as i32))
 }
 
 pub fn register_sha256_foreign(env: &mut HostEnv) {
