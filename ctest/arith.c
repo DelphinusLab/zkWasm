87f982c5a264016dbea732adf97e07d940c0dc6f
sinka <xgao@zoyoe.com>
2022-12-01 01:06:55 +1100

    BUGFIX: fix size of public inputs

--- ctest/arith.c
+++ ctest/arith.c
@@ -1,8 +1,14 @@
 #include <stdint.h>
+void assert(int cond)
+{
+    if (!cond) __builtin_unreachable();
+}
+
 unsigned long long wasm_input(int);
 __attribute__((visibility("default")))
 int zkmain() {
     uint32_t a = (uint32_t)wasm_input(1);
     uint32_t b = (uint32_t)wasm_input(1);
-    return a+b;
+    assert(a > b);
+    return a + b;
 }
