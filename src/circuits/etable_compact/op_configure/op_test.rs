a8928332df508e7597b58539900fcea67a985344
Zhang Junyu <junyu92@gmail.com>
2023-01-18 19:26:59 +0800

    feat: support i64eqz

--- src/circuits/etable_compact/op_configure/op_test.rs
+++ src/circuits/etable_compact/op_configure/op_test.rs
@@ -18,8 +18,8 @@ use specs::{itable::TestOp, mtable::VarType};
 use specs::{itable::OPCODE_ARG1_SHIFT, step::StepInfo};
 
 pub struct TestConfig {
-    // vtype
-    is_four_bytes: BitCell,
+    is_i64: BitCell,
+    value_inv: UnlimitedCell,
 
     value: U64Cell,
     res: BitCell,
@@ -38,18 +38,25 @@ impl<F: FieldExt> EventTableOpcodeConfigBuilder<F> for TestConfigBuilder {
         let value = common.alloc_u64();
         let res = common.alloc_bit_value();
 
-        let is_four_bytes = common.alloc_bit_value();
+        let is_i64 = common.alloc_bit_value();
+        let value_inv = common.alloc_unlimited_value();
 
         let lookup_stack_read = common.alloc_mtable_lookup();
         let lookup_stack_write = common.alloc_mtable_lookup();
 
         constraint_builder.push(
-            "test op",
-            Box::new(move |meta| vec![res.expr(meta) * value.expr(meta)]),
+            "test op value_is_zero",
+            Box::new(move |meta| {
+                vec![
+                    res.expr(meta) * value.expr(meta),
+                    value.expr(meta) * value_inv.expr(meta) - constant_from!(1) + res.expr(meta),
+                ]
+            }),
         );
 
         Box::new(TestConfig {
-            is_four_bytes,
+            is_i64,
+            value_inv,
             value,
             res,
             lookup_stack_read,
@@ -60,9 +67,8 @@ impl<F: FieldExt> EventTableOpcodeConfigBuilder<F> for TestConfigBuilder {
 
 impl<F: FieldExt> EventTableOpcodeConfig<F> for TestConfig {
     fn opcode(&self, meta: &mut VirtualCells<'_, F>) -> Expression<F> {
-        let vtype = self.is_four_bytes.expr(meta) * constant_from!(VarType::I32 as u64)
-            + (constant_from!(1) - self.is_four_bytes.expr(meta))
-                * constant_from!(VarType::I64 as u64);
+        let vtype = self.is_i64.expr(meta) * constant_from!(VarType::I64 as u64)
+            + (constant_from!(1) - self.is_i64.expr(meta)) * constant_from!(VarType::I32 as u64);
 
         constant!(bn_to_field(
             &(BigUint::from(OpcodeClass::Test as u64) << OPCODE_CLASS_SHIFT)
@@ -83,11 +89,11 @@ impl<F: FieldExt> EventTableOpcodeConfig<F> for TestConfig {
                 value,
                 result,
             } => {
-                match vtype {
-                    VarType::I32 => self.is_four_bytes.assign(ctx, true)?,
-                    VarType::I64 => (),
-                }
+                self.is_i64.assign(ctx, vtype == VarType::I64)?;
+
                 self.value.assign(ctx, value)?;
+                self.value_inv
+                    .assign(ctx, F::from(value).invert().unwrap_or(F::zero()))?;
                 self.res.assign(ctx, result == 1)?;
 
                 self.lookup_stack_read.assign(
@@ -133,9 +139,8 @@ impl<F: FieldExt> EventTableOpcodeConfig<F> for TestConfig {
                 common_config.eid(meta),
                 constant_from!(1),
                 common_config.sp(meta) + constant_from!(1),
-                self.is_four_bytes.expr(meta) * constant_from!(VarType::I32)
-                    + (constant_from!(1) - self.is_four_bytes.expr(meta))
-                        * constant_from!(VarType::I64),
+                (constant_from!(1) - self.is_i64.expr(meta)) * constant_from!(VarType::I32)
+                    + self.is_i64.expr(meta) * constant_from!(VarType::I64),
                 self.value.expr(meta),
             )),
 
@@ -157,27 +162,24 @@ mod tests {
     use crate::test::test_circuit_noexternal;
 
     #[test]
-    fn test_i32_eqz_1_ok() {
+    fn test_eqz() {
         let textual_repr = r#"
                 (module
                     (func (export "test")
                       (i32.const 0)
                       (i32.eqz)
                       (drop)
-                    )
-                   )
-                "#;
 
-        test_circuit_noexternal(textual_repr).unwrap()
-    }
-
-    #[test]
-    fn test_i32_eqz_2_ok() {
-        let textual_repr = r#"
-                (module
-                    (func (export "test")
                       (i32.const 1)
                       (i32.eqz)
+                      (drop)
+
+                      (i64.const 0)
+                      (i64.eqz)
+                      (drop)
+
+                      (i64.const 1)
+                      (i64.eqz)
                       (drop)
                     )
                    )
