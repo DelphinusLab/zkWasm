4a2ada43d816cde734cbdebff893e747a3abd3a3
hzx <pku_huangzx@163.com>
2023-02-06 19:21:38 +0800

    combine op bin bit test

--- src/circuits/etable_compact/op_configure/op_bin_bit.rs
+++ src/circuits/etable_compact/op_configure/op_bin_bit.rs
@@ -256,102 +256,44 @@ mod tests {
     use crate::test::test_circuit_noexternal;
 
     #[test]
-    fn test_i32_xor() {
+    fn test_bin_bit() {
         let textual_repr = r#"
                 (module
                     (func (export "test")
-                      (i32.const 1)
-                      (i32.const 1)
-                      i32.xor
-                      drop
-                      (i32.const 21)
-                      (i32.const 31)
-                      i32.xor
-                      drop
-                    )
-                   )
-                "#;
+                        (i32.const 1)
+                        (i32.const 1)
+                        (i32.xor)
+                        (drop)
+                        (i32.const 21)
+                        (i32.const 31)
+                        (i32.xor)
+                        (drop)
+                        (i64.const 1)
+                        (i64.const 1)
+                        (i64.xor)
+                        (drop)
+                        (i64.const 21)
+                        (i64.const 31)
+                        (i64.xor)
+                        (drop)
 
-        test_circuit_noexternal(textual_repr).unwrap()
-    }
+                        (i32.const 21)
+                        (i32.const 31)
+                        (i32.and)
+                        (drop)
+                        (i64.const 21)
+                        (i64.const 31)
+                        (i64.and)
+                        (drop)
 
-    #[test]
-    fn test_i32_and1() {
-        let textual_repr = r#"
-                (module
-                    (func (export "test")
-                      (i32.const 21)
-                      (i32.const 31)
-                      i32.and
-                      drop
-                    )
-                   )
-                "#;
-
-        test_circuit_noexternal(textual_repr).unwrap()
-    }
-
-    #[test]
-    fn test_i32_or1() {
-        let textual_repr = r#"
-                (module
-                    (func (export "test")
-                      (i32.const 21)
-                      (i32.const 31)
-                      i32.or
-                      drop
-                    )
-                   )
-                "#;
-
-        test_circuit_noexternal(textual_repr).unwrap()
-    }
-
-    #[test]
-    fn test_i64_xor() {
-        let textual_repr = r#"
-                (module
-                    (func (export "test")
-                      (i64.const 1)
-                      (i64.const 1)
-                      i64.xor
-                      drop
-                      (i64.const 21)
-                      (i64.const 31)
-                      i64.xor
-                      drop
-                    )
-                   )
-                "#;
-
-        test_circuit_noexternal(textual_repr).unwrap()
-    }
-
-    #[test]
-    fn test_i64_and1() {
-        let textual_repr = r#"
-                (module
-                    (func (export "test")
-                      (i64.const 21)
-                      (i64.const 31)
-                      i64.and
-                      drop
-                    )
-                   )
-                "#;
-
-        test_circuit_noexternal(textual_repr).unwrap()
-    }
-
-    #[test]
-    fn test_i64_or1() {
-        let textual_repr = r#"
-                (module
-                    (func (export "test")
-                      (i64.const 21)
-                      (i64.const 31)
-                      i64.or
-                      drop
+                        (i32.const 21)
+                        (i32.const 31)
+                        (i32.or)
+                        (drop)
+                        (i64.const 21)
+                        (i64.const 31)
+                        (i64.or)
+                        (drop)
                     )
                    )
                 "#;
