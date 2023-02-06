b5b237daa6d9c43e29814a0d1d6feed39c44a95c
hzx <pku_huangzx@163.com>
2023-02-06 19:55:49 +0800

    combine op load test

--- src/circuits/etable_compact/op_configure/op_load.rs
+++ src/circuits/etable_compact/op_configure/op_load.rs
@@ -543,95 +543,28 @@ mod tests {
     use crate::test::test_circuit_noexternal;
 
     #[test]
-    fn test_load() {
-        let textual_repr = r#"
-                (module
-                    (memory $0 1)
-                    (data (i32.const 0) "\ff\00\00\00\fe\00\00\00")
-                    (func (export "test")
-                      (i32.const 0)
-                      (i32.load offset=0)
-                      (drop)
-                      (i32.const 4)
-                      (i32.load offset=0)
-                      (drop)
-                    )
-                   )
-                "#;
-
-        test_circuit_noexternal(textual_repr).unwrap();
-    }
-
-    #[test]
-    fn test_load_64() {
-        let textual_repr = r#"
-                (module
-                    (memory $0 1)
-                    (data (i32.const 0) "\ff\00\00\00\fe\00\00\00")
-                    (func (export "test")
-                      (i32.const 0)
-                      (i64.load offset=0)
-                      (drop)
-                      (i32.const 4)
-                      (i64.load offset=4)
-                      (drop)
-                    )
-                   )
-                "#;
-
-        test_circuit_noexternal(textual_repr).unwrap();
-    }
-
-    #[test]
-    fn test_load_32_8s() {
-        let textual_repr = r#"
-                (module
-                    (memory $0 1)
-                    (data (i32.const 0) "\ff\00\00\00\fe\00\00\00")
-                    (func (export "test")
-                      (i32.const 0)
-                      (i32.load8_s offset=0)
-                      (drop)
-                    )
-                   )
-                "#;
-
-        test_circuit_noexternal(textual_repr).unwrap();
-    }
-
-    #[test]
-    fn test_load_64_8u() {
-        let textual_repr = r#"
-                (module
-                    (memory $0 1)
-                    (data (i32.const 0) "\ff\00\00\00\fe\00\00\00")
-                    (func (export "test")
-                      (i32.const 0)
-                      (i64.load8_u offset=0)
-                      (drop)
-                    )
-                   )
-                "#;
-
-        test_circuit_noexternal(textual_repr).unwrap();
-    }
-
-    #[test]
-    fn test_load_64_cross() {
+    fn test_load_cross() {
         let textual_repr = r#"
                 (module
                     (memory $0 1)
                     (data (i32.const 0) "\ff\00\00\00\fe\00\00\00\fd\00\00\00\fc\00\00\00")
                     (func (export "test")
-                      (i32.const 4)
-                      (i64.load offset=0)
-                      (drop)
-                      (i32.const 6)
-                      (i64.load32_u offset=0)
-                      (drop)
-                      (i32.const 7)
-                      (i64.load16_u offset=0)
-                      (drop)
+                        (i32.const 4)
+                        (i64.load offset=0)
+                        (drop)
+                        (i32.const 6)
+                        (i64.load32_u offset=0)
+                        (drop)
+                        (i32.const 7)
+                        (i64.load16_u offset=0)
+                        (drop)
+
+                        (i32.const 6)
+                        (i32.load offset=0)
+                        (drop)
+                        (i32.const 7)
+                        (i32.load16_u offset=0)
+                        (drop)
                     )
                    )
                 "#;
@@ -640,7 +573,7 @@ mod tests {
     }
 
     #[test]
-    fn test_all_load() {
+    fn test_load_normal() {
         let textual_repr = r#"
                 (module
                     (memory $0 1)
@@ -648,6 +581,9 @@ mod tests {
                     (func (export "test")
                         (i32.const 0)
                         (i64.load offset=0)
+                        (drop)
+                        (i32.const 4)
+                        (i64.load offset=4)
                         (drop)
                         (i32.const 0)
                         (i64.load32_u offset=0)
@@ -669,6 +605,9 @@ mod tests {
                         (drop)
 
                         (i32.const 0)
+                        (i32.load offset=0)
+                        (drop)
+                        (i32.const 4)
                         (i32.load offset=0)
                         (drop)
                         (i32.const 0)
