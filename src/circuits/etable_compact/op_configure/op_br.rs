b4a9f994f653db820cd98d0ed1237261d3c63427
Zhang Junyu <junyu92@gmail.com>
2023-02-24 16:05:57 +0000

    add br test

--- src/circuits/etable_compact/op_configure/op_br.rs
+++ src/circuits/etable_compact/op_configure/op_br.rs
@@ -177,39 +177,3 @@ impl<F: FieldExt> EventTableOpcodeConfig<F> for BrConfig {
         Some(self.dst_pc.expr(meta))
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use crate::test::test_circuit_noexternal;
-
-    #[test]
-    fn test_br_ok() {
-        let textual_repr = r#"
-        (module
-            (func (export "test")
-              (block
-                br 0
-              )
-            )
-           )
-        "#;
-
-        test_circuit_noexternal(textual_repr).unwrap();
-    }
-
-    #[test]
-    fn test_br_drop_ok() {
-        let textual_repr = r#"
-        (module
-            (func (export "test")
-              (block
-                (i32.const 0)
-                br 0
-              )
-            )
-           )
-        "#;
-
-        test_circuit_noexternal(textual_repr).unwrap();
-    }
-}
