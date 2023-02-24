b6bfa5d5ca493f2c7491deb0a243154d0793967d
Zhang Junyu <junyu92@gmail.com>
2023-02-24 16:11:45 +0000

    add rel test

---
+++ src/test/test_wasm_instructions/op_rel.rs
@@ -0,0 +1,54 @@
+use std::vec;
+
+use crate::test::test_circuit_noexternal;
+
+#[test]
+fn test_op_rel() {
+    let tys = vec![
+        ("i32", vec!["0", "1", "2", "-1", "-2", "0x80000000"]),
+        (
+            "i64",
+            vec![
+                "0",
+                "1",
+                "2",
+                "-1",
+                "-2",
+                "-0x100000001",
+                "-0x100000002",
+                "0x100000001",
+                "0x100000002",
+                "0x8000000000000000",
+            ],
+        ),
+    ];
+    let ops = vec![
+        "gt_u", "ge_u", "lt_u", "le_u", "eq", "ne", "gt_s", "ge_s", "lt_s", "le_s",
+    ];
+
+    let mut textual_repr = r#"
+            (module
+                (func (export "test")"#
+        .to_owned();
+
+    for (t, values) in tys {
+        for op in ops.iter() {
+            for l in values.iter() {
+                for r in values.iter() {
+                    textual_repr = format!(
+                        r#"{}
+                            ({}.const {})
+                            ({}.const {})
+                            ({}.{})
+                            (drop)
+                            "#,
+                        textual_repr, t, l, t, r, t, op
+                    );
+                }
+            }
+        }
+    }
+
+    textual_repr = format!("{}))", textual_repr);
+    test_circuit_noexternal(&textual_repr).unwrap()
+}
