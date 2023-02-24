0c726f2761d96ca2d0d5a866ded0455d19a458ca
Heng Zhang <shindar90@gmail.com>
2023-02-24 16:45:54 +0800

    op_global_get

--- src/circuits/etable_v2/op_configure/op_global_get.rs
+++ src/circuits/etable_v2/op_configure/op_global_get.rs
@@ -21,71 +21,67 @@ use specs::{
     etable::EventTableEntry,
     itable::{OpcodeClass, OPCODE_ARG0_SHIFT, OPCODE_CLASS_SHIFT},
     mtable::{LocationType, VarType},
-    step::StepInfo,
+    step::StepInfo, encode::opcode::encode_global_get,
 };
 
-pub struct LocalTeeConfig<F: FieldExt> {
-    offset_cell: AllocatedCommonRangeCell<F>,
+pub struct GlobalGetConfig<F: FieldExt> {
+    idx_cell: AllocatedCommonRangeCell<F>,
     is_i32_cell: AllocatedBitCell<F>,
     value_cell: AllocatedU64Cell<F>,
-    memory_table_lookup_stack_read: AllocatedMemoryTableLookupReadCell<F>,
+    memory_table_lookup_global_read: AllocatedMemoryTableLookupReadCell<F>,
     memory_table_lookup_stack_write: AllocatedMemoryTableLookupWriteCell<F>,
 }
 
-pub struct LocalTeeConfigBuilder {}
+pub struct GlobalGetConfigBuilder {}
 
-impl<F: FieldExt> EventTableOpcodeConfigBuilder<F> for LocalTeeConfigBuilder {
+impl<F: FieldExt> EventTableOpcodeConfigBuilder<F> for GlobalGetConfigBuilder {
     fn configure(
         common_config: &EventTableCommonConfig<F>,
         allocator: &mut EventTableCellAllocator<F>,
         constraint_builder: &mut ConstraintBuilder<F>,
     ) -> Box<dyn EventTableOpcodeConfig<F>> {
         let is_i32_cell = allocator.alloc_bit_cell();
-        let offset_cell = allocator.alloc_common_range_cell();
+        let idx_cell = allocator.alloc_common_range_cell();
         let value_cell = allocator.alloc_u64_cell();
 
         let sp_cell = common_config.sp_cell;
         let eid_cell = common_config.eid_cell;
 
-        let memory_table_lookup_stack_read = allocator.alloc_memory_table_lookup_read_cell(
-            "op_local_tee stack read",
+        let memory_table_lookup_global_read = allocator.alloc_memory_table_lookup_read_cell(
+            "op_global_get global read",
             constraint_builder,
             eid_cell,
-            move |_| constant_from!(LocationType::Stack as u64),
-            move |meta| sp_cell.expr(meta) + constant_from!(1),
+            move |_| constant_from!(LocationType::Global as u64),
+            move |meta| idx_cell.expr(meta),
             move |meta| is_i32_cell.expr(meta),
             move |meta| value_cell.u64_cell.expr(meta),
             move |_| constant_from!(1),
         );
 
         let memory_table_lookup_stack_write = allocator.alloc_memory_table_lookup_write_cell(
-            "op_local_tee stack write",
+            "op_global_get stack write",
             constraint_builder,
             eid_cell,
             move |_| constant_from!(LocationType::Stack as u64),
             move |meta| sp_cell.expr(meta),
-            move |meta| is_i32_cell.expr(meta) + offset_cell.expr(meta),
+            move |meta| is_i32_cell.expr(meta),
             move |meta| value_cell.u64_cell.expr(meta),
             move |_| constant_from!(1),
         );
 
-        Box::new(LocalTeeConfig {
-            offset_cell,
+        Box::new(GlobalGetConfig {
+            idx_cell,
             is_i32_cell,
             value_cell,
-            memory_table_lookup_stack_read,
+            memory_table_lookup_global_read,
             memory_table_lookup_stack_write,
         })
     }
 }
 
-impl<F: FieldExt> EventTableOpcodeConfig<F> for LocalTeeConfig<F> {
+impl<F: FieldExt> EventTableOpcodeConfig<F> for GlobalGetConfig<F> {
     fn opcode(&self, meta: &mut VirtualCells<'_, F>) -> Expression<F> {
-        constant!(bn_to_field(
-            &(BigUint::from(OpcodeClass::LocalTee as u64) << OPCODE_CLASS_SHIFT)
-        )) + self.is_i32_cell.expr(meta)
-            * constant!(bn_to_field(&(BigUint::from(1u64) << OPCODE_ARG0_SHIFT)))
-            + self.offset_cell.expr(meta)
+        encode_global_get(self.idx_cell.expr(meta))
     }
 
     fn assign(
@@ -95,22 +91,20 @@ impl<F: FieldExt> EventTableOpcodeConfig<F> for LocalTeeConfig<F> {
         entry: &EventTableEntryWithMemoryInfo,
     ) -> Result<(), Error> {
         match &entry.eentry.step_info {
-            StepInfo::SetLocal {
-                vtype,
-                depth,
-                value,
+            StepInfo::GetGlobal {
+                idx, vtype, value, ..
             } => {
+                self.idx_cell.assign(ctx, F::from(*idx as u64))?;
                 self.is_i32_cell.assign(ctx, F::from(*vtype as u64))?;
                 self.value_cell.assign(ctx, *value)?;
-                self.offset_cell.assign(ctx, F::from(*depth as u64))?;
 
-                self.memory_table_lookup_stack_read.assign(
+                self.memory_table_lookup_global_read.assign(
                     ctx,
                     entry.memory_rw_entires[0].start_eid,
                     step.current.eid,
                     entry.memory_rw_entires[0].end_eid,
-                    step.current.sp + 1,
-                    LocationType::Stack,
+                    *idx,
+                    LocationType::Global,
                     *vtype == VarType::I32,
                     *value,
                 )?;
@@ -119,7 +113,7 @@ impl<F: FieldExt> EventTableOpcodeConfig<F> for LocalTeeConfig<F> {
                     ctx,
                     step.current.eid,
                     entry.memory_rw_entires[0].end_eid,
-                    step.current.sp + depth,
+                    step.current.sp,
                     LocationType::Stack,
                     *vtype == VarType::I32,
                     *value,
@@ -130,6 +124,10 @@ impl<F: FieldExt> EventTableOpcodeConfig<F> for LocalTeeConfig<F> {
 
             _ => unreachable!(),
         }
+    }
+
+    fn sp_diff(&self, _meta: &mut VirtualCells<'_, F>) -> Option<Expression<F>> {
+        Some(constant!(-F::one()))
     }
 
     fn mops(&self, _meta: &mut VirtualCells<'_, F>) -> Option<Expression<F>> {
