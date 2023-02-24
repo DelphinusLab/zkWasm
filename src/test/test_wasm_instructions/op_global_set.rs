769e14e666b7303c5ca9eacef7e7e875466d636a
Zhang Junyu <junyu92@gmail.com>
2023-02-24 14:18:02 +0000

    add global_set test

---
+++ src/test/test_wasm_instructions/op_global_set.rs
@@ -0,0 +1,21 @@
+use crate::test::test_circuit_noexternal;
+
+
+#[test]
+fn test_global_set() {
+    let textual_repr = r#"
+        (module
+            (global $global_i32 (mut i32) (i32.const 10))
+            (global $global_i64 (mut i32) (i32.const 10))
+
+            (func (export "test")
+                (i32.const 0)
+                (global.set $global_i32)
+                (i32.const 0)
+                (global.set $global_i64)
+            )
+        )
+    "#;
+
+    test_circuit_noexternal(textual_repr).unwrap()
+}
