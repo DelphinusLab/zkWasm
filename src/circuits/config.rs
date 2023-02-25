726b009e40661390a549ec6d0ad89decc39257e8
Heng Zhang <shindar90@gmail.com>
2023-02-25 22:11:51 +0800

    enable more rows

--- src/circuits/config.rs
+++ src/circuits/config.rs
@@ -8,17 +8,17 @@ lazy_static! {
     static ref ZKWASM_K: Mutex<u32> =
         Mutex::new(env::var("ZKWASM_K").map_or(MIN_K, |k| k.parse().unwrap()));
     pub(super) static ref ZKWASM_TABLE_DENOMINATOR: u32 =
-        env::var("ZKWASM_TABLE_DENOMINATOR").map_or(8, |k| k.parse().unwrap());
+        env::var("ZKWASM_TABLE_DENOMINATOR").map_or(32, |k| k.parse().unwrap());
     static ref ZKWASM_ETABLE_RATIO: u32 =
-        env::var("ZKWASM_ETABLE_RATIO").map_or(6, |k| k.parse().unwrap());
+        env::var("ZKWASM_ETABLE_RATIO").map_or(31, |k| k.parse().unwrap());
     static ref ZKWASM_MTABLE_RATIO: u32 =
-        env::var("ZKWASM_MTABLE_RATIO").map_or(7, |k| k.parse().unwrap());
+        env::var("ZKWASM_MTABLE_RATIO").map_or(31, |k| k.parse().unwrap());
     static ref ZKWASM_JTABLE_RATIO: u32 =
-        env::var("ZKWASM_JTABLE_RATIO").map_or(6, |k| k.parse().unwrap());
+        env::var("ZKWASM_JTABLE_RATIO").map_or(31, |k| k.parse().unwrap());
     pub(super) static ref ZKWASM_FOREIGN_CALL_TABLE_RATIO: u32 =
-        env::var("ZKWASM_JTABLE_RATIO").map_or(6, |k| k.parse().unwrap());
+        env::var("ZKWASM_FOREIGN_CALL_TABLE_RATIO").map_or(31, |k| k.parse().unwrap());
     static ref ZKWASM_SHA256_RATIO: u32 =
-        env::var("ZKWASM_SHA256_RATIO").map_or(6, |k| k.parse().unwrap());
+        env::var("ZKWASM_SHA256_RATIO").map_or(31, |k| k.parse().unwrap());
 }
 
 pub fn set_zkwasm_k(k: u32) {
