b5fb39dab64fdf8eedd3e377b3f57315bc271e05
Zhang Junyu <junyu92@gmail.com>
2023-02-26 12:34:05 +0000

    add rlp simple test

---
+++ src/test/test_rlp_simple.rs
@@ -0,0 +1,81 @@
+use crate::{
+    foreign::wasm_input_helper::runtime::register_wasm_input_foreign,
+    runtime::{
+        host::host_env::HostEnv, wasmi_interpreter::Execution, ExecutionResult, WasmInterpreter,
+    },
+};
+
+use anyhow::Result;
+use halo2_proofs::pairing::bn256::Fr as Fp;
+use specs::Tables;
+use std::fs::{self};
+use wasmi::ImportsBuilder;
+
+use super::test_circuit_with_env;
+
+fn build_test() -> Result<(ExecutionResult<wasmi::RuntimeValue>, Vec<u64>)> {
+    let public_inputs = vec![2, 2];
+    let private_inputs = vec![];
+
+    let wasm = fs::read("wasm/rlp_simple.wasm").unwrap();
+    let module = wasmi::Module::from_buffer(&wasm).expect("failed to load wasm");
+
+    let mut env = HostEnv::new();
+    register_wasm_input_foreign(&mut env, public_inputs.clone(), private_inputs);
+    env.finalize();
+
+    let imports = ImportsBuilder::new().with_resolver("env", &env);
+    let compiler = WasmInterpreter::new();
+    let compiled_module = compiler
+        .compile(
+            &module,
+            &imports,
+            &env.function_description_table(),
+            "zkmain",
+        )
+        .unwrap();
+    let execution_result = compiled_module.run(&mut env)?;
+
+    Ok((execution_result, public_inputs))
+}
+
+mod tests {
+    use super::*;
+    use crate::{
+        circuits::{config::set_zkwasm_k, ZkWasmCircuitBuilder},
+        test::run_test_circuit,
+    };
+    use halo2_proofs::pairing::bn256::Fr as Fp;
+    use rusty_fork::rusty_fork_test;
+
+    rusty_fork_test! {
+        #[test]
+        fn test_rlp_simple_mock() {
+            set_zkwasm_k(20);
+
+            let (execution_result, public_inputs) = build_test().unwrap();
+
+            run_test_circuit(
+                execution_result,
+                public_inputs.into_iter().map(|v| Fp::from(v)).collect(),
+            )
+            .unwrap();
+        }
+    }
+
+    rusty_fork_test! {
+        #[test]
+        fn test_rlp_bench() {
+            set_zkwasm_k(20);
+
+            let (execution_result, public_inputs) = build_test().unwrap();
+
+            let builder = ZkWasmCircuitBuilder {
+                fid_of_entry: execution_result.fid_of_entry,
+                tables: execution_result.tables,
+            };
+
+            builder.bench(public_inputs.into_iter().map(|v| Fp::from(v)).collect())
+        }
+    }
+}
