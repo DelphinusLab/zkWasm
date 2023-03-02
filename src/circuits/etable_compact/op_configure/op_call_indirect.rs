0ac2da7c606cb536361dcd1a07bc508d6e88d9dc
Zhang Junyu <junyu92@gmail.com>
2023-03-02 14:45:52 +0000

    remove duplicate tests

--- src/circuits/etable_compact/op_configure/op_call_indirect.rs
+++ src/circuits/etable_compact/op_configure/op_call_indirect.rs
@@ -202,36 +202,3 @@ impl<F: FieldExt> EventTableOpcodeConfig<F> for CallIndirectConfig {
         ))
     }
 }
-
-#[cfg(test)]
-
-mod tests {
-    use crate::test::test_circuit_noexternal;
-
-    #[test]
-    fn test_call_indirect() {
-        let textual_repr = r#"
-            (module
-                (type (;0;) (func (param i32 i32) (result i32)))
-                (func (;0;) (type 0) (param i32 i32) (result i32)
-                    local.get 0
-                    local.get 1
-                    i32.add)
-                (func (;1;) (type 0) (param i32 i32) (result i32)
-                    local.get 0
-                    local.get 1
-                    i32.sub)
-                (func (;2;) (result i32)
-                    i32.const 1
-                    i32.const 2
-                    i32.const 1
-                    call_indirect (type 0))
-                (table (;0;) 2 2 funcref)
-                (export "test" (func 2))
-                (elem (;0;) (i32.const 0) func 0 1)
-            )
-        "#;
-
-        test_circuit_noexternal(textual_repr).unwrap()
-    }
-}
--- src/circuits/etable_compact/op_configure/op_memory_size.rs
+++ src/circuits/etable_compact/op_configure/op_memory_size.rs
@@ -97,24 +97,3 @@ impl<F: FieldExt> EventTableOpcodeConfig<F> for MemorySizeConfig {
         }
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use crate::test::test_circuit_noexternal;
-
-    #[test]
-    fn test_memory_size() {
-        let textual_repr = r#"
-                (module
-                    (memory 2)
-
-                    (func (export "test")
-                      (memory.size)
-                      (drop)
-                    )
-                   )
-                "#;
-
-        test_circuit_noexternal(textual_repr).unwrap()
-    }
-}
