d081a7b6ead2698e9270ef103fe76dbc5d76d798
sinka <xgao@zoyoe.com>
2023-06-20 12:57:48 +1000

    add comment for hash sha256 hostapi

--- src/foreign/hash_helper/sha256.rs
+++ src/foreign/hash_helper/sha256.rs
@@ -9,6 +9,33 @@ use zkwasm_host_circuits::host::ForeignInst::{
 
 use sha2::Sha256;
 
+
+/// Foreign functions that supports the following C code library
+///
+/// void sha256(uint8_t* data, uint32_t size, uint64_t* r)
+/// {
+///     int i;
+///     sha256_new(size);
+///     for(i=0; i<size; i=i+8) {
+///         uint64_t* a = (uint64_t *)(&data[i*8]);
+///         sha256_push(*a);
+///     }
+///     int offset = i;
+///     uint8_t buf[8] = {0};
+///     for(;i<size;i++) {
+///         buf[i-offset] = data[i];
+///     }
+///     sha256_push(*((uint64_t *)buf));
+///     r[0] = sha256_finalize();
+///     r[1] = sha256_finalize();
+///     r[2] = sha256_finalize();
+///     r[3] = sha256_finalize();
+///     wasm_dbg(r[0]);
+///     wasm_dbg(r[1]);
+///     wasm_dbg(r[2]);
+///     wasm_dbg(r[3]);
+/// }
+
 struct Generator {
     pub cursor: usize,
     pub values: Vec<u64>,
