d4065346a2fb0534af06034708128d17c50644a1
Zhang Junyu <junyu92@gmail.com>
2023-07-05 19:55:58 +0800

    mask solidity-aggregate-verifier if sol does not exist

--- test_cli_checksum.sh
+++ test_cli_checksum.sh
@@ -18,4 +18,6 @@ RUST_LOG=info cargo run --release --features cuda --features checksum -- -k 18 -
 RUST_LOG=info cargo run --release --features cuda --features checksum -- -k 18 --function zkmain --output ./output --wasm wasm/wasm_output.wasm aggregate-prove --public 133:i64 --public 2:i64
 
 RUST_LOG=info cargo run --release --features cuda --features checksum -- -k 18 --function zkmain --output ./output --wasm wasm/wasm_output.wasm aggregate-verify --proof output/aggregate-circuit.0.transcript.data --instances output/aggregate-circuit.0.instance.data
-RUST_LOG=info cargo run --release --features cuda --features checksum -- -k 18 --function zkmain --output ./output --wasm wasm/wasm_output.wasm solidity-aggregate-verifier --proof output/aggregate-circuit.0.transcript.data  --instances output/aggregate-circuit.0.instance.data
+if [ -d "sol" ]; then
+  RUST_LOG=info cargo run --release --features cuda --features checksum -- -k 18 --function zkmain --output ./output --wasm wasm/wasm_output.wasm solidity-aggregate-verifier --proof output/aggregate-circuit.0.transcript.data  --instances output/aggregate-circuit.0.instance.data
+fi
