301d12b262babe1c6135167c870f0d2477b0752f
sinka <xgao@zoyoe.com>
2023-07-05 12:30:21 +1000

    fix hash hosts

--- src/foreign/hash_helper/poseidon.rs
+++ src/foreign/hash_helper/poseidon.rs
@@ -48,6 +48,9 @@ impl Generator {
     fn gen(&mut self) -> u64 {
         let r = self.values[self.cursor];
         self.cursor += 1;
+        if self.cursor == 4 {
+            self.cursor = 0;
+        }
         r
     }
 }
@@ -62,8 +65,8 @@ fn new_reduce(rules: Vec<ReduceRule<Fr>>) -> Reduce<Fr> {
 struct PoseidonContext {
     pub hasher: Option<Poseidon<Fr, T, RATE>>,
     pub generator: Generator,
+    pub buf: Vec<Fr>,
     pub fieldreducer:Reduce<Fr>,
-    pub size: usize,
 }
 
 impl PoseidonContext {
@@ -71,11 +74,11 @@ impl PoseidonContext {
         PoseidonContext {
             hasher: None,
             fieldreducer:new_reduce(vec![ReduceRule::Field(Fr::zero(), 64)]),
+            buf: vec![],
             generator: Generator {
                 cursor: 0,
                 values: vec![],
             },
-            size: 0,
         }
     }
 }
@@ -97,15 +100,12 @@ pub fn register_poseidon_foreign(env: &mut HostEnv) {
         Rc::new(
             |context: &mut dyn ForeignContext, args: wasmi::RuntimeArgs| {
                 let context = context.downcast_mut::<PoseidonContext>().unwrap();
-                let hasher = context.hasher.as_mut().map_or({
-                    Some(gen_hasher())
-                }, |_| {
-                    None
-                });
-                hasher.map(|s| {
-                    context.hasher = Some(s);
-                    context.size = args.nth::<u64>(0) as usize;
-                });
+                println!("buf len is {}", context.buf.len());
+                context.buf = vec![];
+                let new = args.nth::<u64>(0) as usize;
+                if new != 0 {
+                    context.hasher = Some(gen_hasher());
+                }
                 None
             },
         ),
@@ -119,13 +119,10 @@ pub fn register_poseidon_foreign(env: &mut HostEnv) {
         Rc::new(
             |context: &mut dyn ForeignContext, args: wasmi::RuntimeArgs| {
                 let context = context.downcast_mut::<PoseidonContext>().unwrap();
-                context.hasher.as_mut().map(|s| {
-                    context.fieldreducer.reduce(args.nth::<u64>(0) as u64);
-                    if context.fieldreducer.cursor == 0 {
-                        s.update(&[context.fieldreducer.rules[0].field_value().unwrap()]);
-                        context.size -= 1;
-                    }
-                });
+                context.fieldreducer.reduce(args.nth::<u64>(0) as u64);
+                if context.fieldreducer.cursor == 0 {
+                    context.buf.push(context.fieldreducer.rules[0].field_value().unwrap())
+                }
                 None
             },
         ),
@@ -140,14 +137,17 @@ pub fn register_poseidon_foreign(env: &mut HostEnv) {
         Rc::new(
             |context: &mut dyn ForeignContext, _args: wasmi::RuntimeArgs| {
                 let context = context.downcast_mut::<PoseidonContext>().unwrap();
-                assert!(context.size == 0);
-                context.hasher.as_ref().map(|s| {
-                    let dwords:Vec<u8> = s.clone().squeeze().to_repr().to_vec();
-                    context.generator.values = dwords.chunks(8).map(|x| {
-                        u64::from_le_bytes(x.to_vec().try_into().unwrap())
-                    }).collect::<Vec<u64>>();
-                });
-                context.hasher = None;
+                assert!(context.buf.len() == 8);
+                if context.generator.cursor == 0 {
+                    context.hasher.as_ref().map(|s| {
+                        println!("perform hash with {:?}", context.buf);
+                        let r = s.clone().update_exact(&context.buf.clone().try_into().unwrap());
+                        let dwords:Vec<u8> = r.to_repr().to_vec();
+                        context.generator.values = dwords.chunks(8).map(|x| {
+                            u64::from_le_bytes(x.to_vec().try_into().unwrap())
+                        }).collect::<Vec<u64>>();
+                    });
+                }
                 Some(wasmi::RuntimeValue::I64(context.generator.gen() as i64))
             },
         ),
