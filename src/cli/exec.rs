b2067ca6e205102cba360306b4f9627f305882be
Sinka <xgao@zoyoe.com>
2023-07-06 15:32:34 +1000

    add babyjubjub helper functions

--- src/cli/exec.rs
+++ src/cli/exec.rs
@@ -55,6 +55,7 @@ use crate::foreign::ecc_helper::{
     bls381::sum::register_blssum_foreign,
     bn254::pair::register_bn254pair_foreign,
     bn254::sum::register_bn254sum_foreign,
+    jubjub::sum::register_babyjubjubsum_foreign,
 };
 
 const AGGREGATE_PREFIX: &'static str = "aggregate-circuit";
@@ -77,6 +78,7 @@ pub fn compile_image<'a>(
     register_bn254sum_foreign(&mut env);
     register_sha256_foreign(&mut env);
     register_poseidon_foreign(&mut env);
+    register_babyjubjubsum_foreign(&mut env);
     env.finalize();
     let imports = ImportsBuilder::new().with_resolver("env", &env);
 
@@ -140,6 +142,7 @@ fn build_circuit_with_witness(
     register_bn254sum_foreign(&mut env);
     register_sha256_foreign(&mut env);
     register_poseidon_foreign(&mut env);
+    register_babyjubjubsum_foreign(&mut env);
     env.finalize();
     let imports = ImportsBuilder::new().with_resolver("env", &env);
 
