50d8cb9b7ddda54170d15923d7d088fc77e5fd9d
Zhang Junyu <junyu92@gmail.com>
2023-08-11 19:36:25 +0800

    chore: clean unused code

--- src/circuits/test_circuit/mod.rs
+++ src/circuits/test_circuit/mod.rs
@@ -93,7 +93,7 @@ impl<F: FieldExt> Circuit<F> for TestCircuit<F> {
         let mut cols = [(); VAR_COLUMNS].map(|_| meta.advice_column()).into_iter();
 
         let rtable =
-            RangeTableConfig::configure([0; 10].map(|_| meta.lookup_table_column()).into_iter());
+            RangeTableConfig::configure([0; 6].map(|_| meta.lookup_table_column()).into_iter());
         let image_table = ImageTableConfig::configure(meta);
         let mtable = MemoryTableConfig::configure(meta, &mut cols, &rtable, &image_table);
         let jtable = JumpTableConfig::configure(meta, &mut cols);
