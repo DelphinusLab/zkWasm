ffd90dfb83a16e19156f3158c2b146656e2ff022
Zhang Junyu <junyu92@gmail.com>
2023-08-28 14:48:47 +0800

    fix checksum

--- crates/zkwasm/src/foreign/wasm_input_helper/mod.rs
+++ crates/zkwasm/src/foreign/wasm_input_helper/mod.rs
@@ -3,8 +3,6 @@ pub mod etable_op_configure;
 pub mod runtime;
 pub mod test;
 
-pub const RESERVED_INSTANCES_NUMBER: usize = if cfg!(feature = "checksum") { 1 } else { 0 };
-
 enum Op {
     WasmInput = 0,
     WasmOutput = 1,
