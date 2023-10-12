afe8e16b308765d103aa3c9dd7ad807775cad0e8
Zhang Junyu <junyu92@gmail.com>
2023-10-12 18:17:07 +0800

    feat: dump function name for perf monitor

--- crates/specs/src/itable.rs
+++ crates/specs/src/itable.rs
@@ -589,6 +589,7 @@ impl Into<OpcodeClassPlain> for Opcode {
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct InstructionTableEntry {
     pub fid: u32,
+    pub function_name: String,
     pub iid: u32,
     pub opcode: Opcode,
 }
@@ -652,7 +653,12 @@ impl InstructionTable {
         opcodeclass
     }
 
-    pub fn push(&mut self, fid: u32, iid: u32, opcode: Opcode) {
-        self.0.push(InstructionTableEntry { fid, iid, opcode })
+    pub fn push(&mut self, fid: u32, function_name: String, iid: u32, opcode: Opcode) {
+        self.0.push(InstructionTableEntry {
+            fid,
+            function_name,
+            iid,
+            opcode,
+        })
     }
 }
