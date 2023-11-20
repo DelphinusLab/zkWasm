9383a459b9c1b4337f4e7fabc30d2a277652104a
Sinka <xgao@zoyoe.com>
2023-11-20 21:49:29 +1100

    fix: add missing files

---
+++ crates/host/src/host/witness_helper/mod.rs
@@ -0,0 +1,57 @@
+use delphinus_zkwasm::runtime::host::ForeignContext;
+use std::rc::Rc;
+
+use crate::HostEnv;
+use zkwasm_host_circuits::host::ForeignInst::WitnessInsert;
+use zkwasm_host_circuits::host::ForeignInst::WitnessPop;
+
+#[derive (Default)]
+pub struct WitnessContext {
+    pub buf: Vec<u64>,
+}
+
+impl WitnessContext {
+    pub fn witness_insert(&mut self, new: u64) {
+        self.buf.insert(0, new);
+    }
+
+    pub fn witness_pop(&mut self) -> u64 {
+        self.buf.pop().unwrap()
+    }
+}
+
+impl ForeignContext for WitnessContext {}
+
+use specs::external_host_call_table::ExternalHostCallSignature;
+pub fn register_witness_foreign(env: &mut HostEnv) {
+    let foreign_witness_plugin = env
+        .external_env
+        .register_plugin("foreign_witness", Box::new(WitnessContext::default()));
+
+    env.external_env.register_function(
+        "wasm_witness_insert",
+        WitnessInsert as usize,
+        ExternalHostCallSignature::Argument,
+        foreign_witness_plugin.clone(),
+        Rc::new(
+            |context: &mut dyn ForeignContext, args: wasmi::RuntimeArgs| {
+                let context = context.downcast_mut::<WitnessContext>().unwrap();
+                context.witness_insert(args.nth::<u64>(0) as u64);
+                None
+            },
+        ),
+    );
+
+    env.external_env.register_function(
+        "wasm_witness_pop",
+        WitnessPop as usize,
+        ExternalHostCallSignature::Return,
+        foreign_witness_plugin.clone(),
+        Rc::new(
+            |context: &mut dyn ForeignContext, _args: wasmi::RuntimeArgs| {
+                let context = context.downcast_mut::<WitnessContext>().unwrap();
+                Some(wasmi::RuntimeValue::I64(context.witness_pop() as i64))
+            },
+        ),
+    );
+}
