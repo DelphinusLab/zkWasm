6f0eaa3fa210372396920071700745db47d1f1db
Zhang Junyu <junyu92@gmail.com>
2023-12-02 17:08:43 +0800

    feat: replace itable with Vec

--- crates/specs/src/etable.rs
+++ crates/specs/src/etable.rs
@@ -19,7 +19,7 @@ pub struct EventTableEntry {
 
 impl EventTableEntry {
     pub fn get_instruction<'a>(&self, itable: &'a InstructionTable) -> &'a InstructionTableEntry {
-        itable.get(self.fid, self.iid)
+        &itable.get(self.fid, self.iid).as_ref().unwrap()
     }
 }
 
