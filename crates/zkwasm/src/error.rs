61416fdb0be19f7ac8ad600470a0a5db9426641d
Zhang Junyu <junyu92@gmail.com>
2024-04-14 05:28:45 +0000

    fix typo

--- crates/zkwasm/src/error.rs
+++ crates/zkwasm/src/error.rs
@@ -10,8 +10,8 @@ pub enum ExecutionError {}
 pub enum BuildingCircuitError {
     #[error("Only support single slice for non-continuation mode but {0} provided. You could increase K or enable continuation feature.")]
     MultiSlicesNotSupport(usize),
-    #[error("Allocated pages({0}) exceed the limit({1}). Current K is {2}, consider increase the circuit size K.")]
+    #[error("Allocated pages({0}) exceed the limit({1}). Current K is {2}, consider increasing the circuit size K.")]
     PagesExceedLimit(u32, u32, u32),
-    #[error("Etable entries({0}) exceed the limit({1}). Current K is {2}, consider increase the circuit size K.")]
+    #[error("Etable entries({0}) exceed the limit({1}). Current K is {2}, consider increasing the circuit size K.")]
     EtableEntriesExceedLimit(u32, u32, u32),
 }
