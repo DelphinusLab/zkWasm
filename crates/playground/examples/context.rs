090e922d69308b32680eaccf7fe878b7ff4fa8f7
Zhang Junyu <junyu92@gmail.com>
2024-04-29 06:24:23 +0000

    fix playground

--- crates/playground/examples/context.rs
+++ crates/playground/examples/context.rs
@@ -1,41 +1,61 @@
 use anyhow::Result;
-use delphinus_zkwasm::foreign::context::ContextOutput;
+use delphinus_zkwasm::circuits::config::MIN_K;
+use delphinus_zkwasm::loader::slice::Slices;
 use delphinus_zkwasm::loader::TraceBackend;
 use delphinus_zkwasm::loader::ZkWasmLoader;
 use delphinus_zkwasm::runtime::host::default_env::DefaultHostEnvBuilder;
 use delphinus_zkwasm::runtime::host::default_env::ExecutionArg;
-use pairing_bn256::bn256::Bn256;
+use delphinus_zkwasm::runtime::host::HostEnvBuilder;
+use delphinus_zkwasm::runtime::monitor::table_monitor::TableMonitor;
+use pairing_bn256::bn256::Fr;
+
+const K: u32 = MIN_K;
 
 fn main() -> Result<()> {
     let wasm = std::fs::read("wasm/context.wasm")?;
+    let module = ZkWasmLoader::parse_module(&wasm)?;
 
-    let context_in: Vec<u64> = vec![2, 1];
-    let context_outputs = ContextOutput::default();
+    let context_output = {
+        let env = DefaultHostEnvBuilder.create_env(
+            K,
+            ExecutionArg {
+                public_inputs: vec![],
+                private_inputs: vec![],
+                context_inputs: vec![2, 1],
+            },
+        );
 
-    let loader = ZkWasmLoader::<Bn256, ExecutionArg, DefaultHostEnvBuilder>::new(18, wasm, vec![])?;
-    let arg = ExecutionArg {
-        public_inputs: vec![],
-        private_inputs: vec![],
-        context_inputs: context_in,
-        context_outputs: context_outputs.clone(),
+        let mut monitor = TableMonitor::new(K, &vec![], TraceBackend::Memory, &env);
+        let loader = ZkWasmLoader::new(K, env)?;
+
+        let runner = loader.compile(&module, &mut monitor)?;
+        let result = loader.run(runner, &mut monitor)?;
+
+        let slices: Slices<Fr> = Slices::new(K, monitor.into_tables())?;
+        slices.mock_test_all(result.public_inputs_and_outputs())?;
+
+        result.context_outputs
     };
-    let result = loader.run(arg, (), false, TraceBackend::Memory)?;
-    let instances = result.public_inputs_and_outputs();
 
-    let slices = loader.slice(result);
-    slices.mock_test_all(18, instances)?;
+    {
+        let env = DefaultHostEnvBuilder.create_env(
+            K,
+            ExecutionArg {
+                public_inputs: vec![],
+                private_inputs: vec![],
+                context_inputs: context_output.0,
+            },
+        );
 
-    let arg = ExecutionArg {
-        public_inputs: vec![],
-        private_inputs: vec![],
-        context_inputs: context_outputs.0.lock().unwrap().to_vec(),
-        context_outputs: ContextOutput::default(),
-    };
-    let result = loader.run(arg, (), false, TraceBackend::Memory)?;
-    let instances = result.public_inputs_and_outputs();
+        let mut monitor = TableMonitor::new(K, &vec![], TraceBackend::Memory, &env);
+        let loader = ZkWasmLoader::new(K, env)?;
 
-    let slices = loader.slice(result);
-    slices.mock_test_all(18, instances)?;
+        let runner = loader.compile(&module, &mut monitor)?;
+        let result = loader.run(runner, &mut monitor)?;
+
+        let slices: Slices<Fr> = Slices::new(K, monitor.into_tables())?;
+        slices.mock_test_all(result.public_inputs_and_outputs())?;
+    }
 
     Ok(())
 }
--- crates/playground/examples/fibonacci.rs
+++ crates/playground/examples/fibonacci.rs
@@ -1,31 +1,37 @@
 use anyhow::Result;
-use delphinus_zkwasm::foreign::context::ContextOutput;
+use delphinus_zkwasm::circuits::config::MIN_K;
+use delphinus_zkwasm::loader::slice::Slices;
 use delphinus_zkwasm::loader::TraceBackend;
 use delphinus_zkwasm::loader::ZkWasmLoader;
 use delphinus_zkwasm::runtime::host::default_env::DefaultHostEnvBuilder;
 use delphinus_zkwasm::runtime::host::default_env::ExecutionArg;
-use pairing_bn256::bn256::Bn256;
+use delphinus_zkwasm::runtime::host::HostEnvBuilder;
+use delphinus_zkwasm::runtime::monitor::table_monitor::TableMonitor;
+use pairing_bn256::bn256::Fr;
+
+const K: u32 = MIN_K;
 
 fn main() -> Result<()> {
     let wasm = std::fs::read("wasm/fibonacci.wasm")?;
+    let module = ZkWasmLoader::parse_module(&wasm)?;
 
-    let loader = ZkWasmLoader::<Bn256, ExecutionArg, DefaultHostEnvBuilder>::new(18, wasm, vec![])?;
-    let result = loader.run(
+    let env = DefaultHostEnvBuilder.create_env(
+        K,
         ExecutionArg {
             public_inputs: vec![5],
             private_inputs: vec![],
             context_inputs: vec![],
-            context_outputs: ContextOutput::default(),
         },
-        (),
-        false,
-        TraceBackend::Memory,
-    )?;
+    );
+    let mut monitor = TableMonitor::new(K, &vec![], TraceBackend::Memory, &env);
+    let loader = ZkWasmLoader::new(K, env)?;
 
-    let instances = result.public_inputs_and_outputs();
+    let runner = loader.compile(&module, &mut monitor)?;
+    let result = loader.run(runner, &mut monitor)?;
+    let instances = result.public_inputs_and_outputs::<Fr>();
 
-    let slices = loader.slice(result);
-    slices.mock_test_all(18, instances)?;
+    let slices = Slices::new(K, monitor.into_tables())?;
+    slices.mock_test_all(instances)?;
 
     Ok(())
 }
