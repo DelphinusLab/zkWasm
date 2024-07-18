f6c582a7af7529aaa778263eaddad75122b52820
Zhang Junyu <junyu92@gmail.com>
2024-07-18 08:39:06 +0000

    local get

--- crates/zkwasm/src/test/test_wasm_instructions/op_local_get.rs
+++ crates/zkwasm/src/test/test_wasm_instructions/op_local_get.rs
@@ -1,19 +1,18 @@
-use crate::test::test_circuit_noexternal;
+use crate::test::test_instruction;
 
 #[test]
 fn test_local_get() {
     let textual_repr = r#"
             (module
-                (func (export "test")
+                (func (export "zkmain")
                     (local i32 i64)
                     (local.get 0)
                     (drop)
                     (local.get 1)
                     (drop)
-                
                 )
             )
             "#;
 
-    test_circuit_noexternal(textual_repr).unwrap()
+    test_instruction(textual_repr).unwrap()
 }
