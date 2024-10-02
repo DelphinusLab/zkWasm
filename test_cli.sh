239351c57e9d983ec66bf525a739280d93b218ff
rhaoio <richard@rhao.io>
2024-10-02 17:12:27 +0800

    test context

--- test_cli.sh
+++ test_cli.sh
@@ -5,49 +5,20 @@ CLI=./target/release/zkwasm-cli
 set -e
 set -x
 
-CUDA="--features perf"
-
-test_default_cli() {
-    cargo build --release $CUDA
-    rm -rf params/*.data params/*.config output
-    $CLI --params ./params wasm_output setup --wasm ./crates/zkwasm/wasm/wasm_output.wasm
-    $CLI --params ./params wasm_output dry-run --wasm crates/zkwasm/wasm/wasm_output.wasm --public 133:i64 --public 2:i64 --output ./output
-    $CLI --params ./params wasm_output prove --wasm crates/zkwasm/wasm/wasm_output.wasm --public 133:i64 --public 2:i64 --output ./output
-    $CLI --params ./params wasm_output verify --output ./output
-}
-
-test_uniform_circuit_cli() {
-    cargo build --release --features uniform-circuit $CUDA
-    rm -rf params/*.data params/*.config output
-    $CLI --params ./params wasm_output setup
-    $CLI --params ./params wasm_output dry-run --wasm crates/zkwasm/wasm/wasm_output.wasm --public 133:i64 --public 2:i64 --output ./output
-    $CLI --params ./params wasm_output prove --wasm crates/zkwasm/wasm/wasm_output.wasm --public 133:i64 --public 2:i64 --output ./output
-    $CLI --params ./params wasm_output verify --output ./output
-}
-
 test_continuation_cli() {
-    cargo build --release --features continuation $CUDA
+    cargo build --release --features continuation,perf,profile,cuda
     rm -rf params/*.data params/*.config output
-    $CLI --params ./params fibonacci setup
-    $CLI --params ./params fibonacci dry-run --wasm crates/zkwasm/wasm/fibonacci.wasm --public 25:i64 --output ./output
-    $CLI --params ./params fibonacci prove --wasm crates/zkwasm/wasm/fibonacci.wasm --public 25:i64 --output ./output
-    $CLI --params ./params fibonacci verify --output ./output
+    $CLI --params ./params context setup --host standard
+    $CLI --params ./params context dry-run --wasm crates/zkwasm/wasm/rust-sdk-test.wasm --public 25:i64 --private 1:i64 --ctxin 1:i64 --output ./output
+    CUDA_VISIBLE_DEVICES=0 $CLI --params ./params context prove --public 25:i64 --private 1:i64 --ctxin 1:i64 --padding 3 --wasm crates/zkwasm/wasm/rust-sdk-test.wasm --output ./output
+    $CLI --params ./params context verify --output ./output
 }
 
-test_phantom_cli() {
-    cargo build --release $CUDA
-    rm -rf params/*.data params/*.config output
-    $CLI --params ./params wasm_output setup --wasm ./crates/playground/wasm/phantom.wasm --phantom search
-    $CLI --params ./params wasm_output dry-run --wasm crates/playground/wasm/phantom.wasm --public 2:i64 --output ./output
-    $CLI --params ./params wasm_output prove --wasm crates/playground/wasm/phantom.wasm --public 2:i64 --output ./output
-    $CLI --params ./params wasm_output verify --output ./output
-}
 
 #x=50
 #while [ $x -gt 0 ]; do
 #    test_phantom_cli
-    test_default_cli
-    test_uniform_circuit_cli
     test_continuation_cli
 #    x=$(($x-1))
 #done
+
