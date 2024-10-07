b9383c683df0dfe0d0dd986fe0fad8e172eb9c02
Jason <j8012107@gmail.com>
2024-10-07 10:14:13 +0000

    cli: feat: add commit scheme option

--- crates/cli/src/args.rs
+++ crates/cli/src/args.rs
@@ -1,12 +1,30 @@
+use circuits_batcher::args::OpenSchema;
 use serde::Deserialize;
 use serde::Serialize;
 
 #[derive(clap::ArgEnum, Copy, Clone, Debug, Default, Serialize, Deserialize)]
-pub enum HostMode {
+pub(crate) enum HostMode {
     /// Trivial Wasm Host Environment
     #[default]
     Default,
 
     /// Wasm Host Environment with more Zk plugins
     Standard,
+}
+
+#[derive(clap::ArgEnum, Copy, Clone, Debug, Default, Serialize, Deserialize)]
+pub(crate) enum Scheme {
+    Gwc,
+
+    #[default]
+    Shplonk,
+}
+
+impl From<Scheme> for OpenSchema {
+    fn from(val: Scheme) -> OpenSchema {
+        match val {
+            Scheme::Gwc => OpenSchema::GWC,
+            Scheme::Shplonk => OpenSchema::Shplonk,
+        }
+    }
 }
