f7dae714dc2a920cae5053bf90812f1ff0623d82
Maxim Evtush <154841002+maximevtush@users.noreply.github.com>
2025-02-19 13:07:50 +0100

    Update v2.rs

--- crates/zkwasm/src/runtime/monitor/plugins/table/transaction/v2.rs
+++ crates/zkwasm/src/runtime/monitor/plugins/table/transaction/v2.rs
@@ -68,7 +68,7 @@ impl Checkpoint {
     // return value:
     // Ordering::Greater: at least one of transaction is overflow
     // Ordering::Equal: all transactions are full
-    // Ordering::Less : no transaction overflow and at least one of transacion is not full
+    // Ordering::Less : no transaction overflow and at least one of transaction is not full
     fn transactions_group_number_ordering(
         &self,
         applied_transactions_group_number: &HashMap<TransactionId, usize>,
